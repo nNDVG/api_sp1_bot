1506b9c1201aff1bff8f314dfc37dfc72dcca902
nNDVG <grishinkrd@yandex.ru>
2020-09-11 13:29:10 +0300

    pytest 100%

--- homework.py
+++ homework.py
@@ -13,8 +13,9 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+    if status is not 'approved':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,20 +23,23 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    params = {'from_date': current_timestamp}
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    homework_statuses = requests.get(url, headers=headers, params=params)
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())   # начальное значение timestamp
 
     while True:
+        print(current_timestamp)
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
@@ -51,3 +55,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
