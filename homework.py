42c40e35e107a416d5437cf8ef81b06153aae21f
Dmitry Grishin <67337823+nNDVG@users.noreply.github.com>
2021-03-13 18:29:19 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
